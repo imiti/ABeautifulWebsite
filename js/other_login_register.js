b7e7c1383f0b86840f9d7c214dc6b0ae2874f836
李青云 <rocklee.love@163.com>
2013-10-18 00:48:55 +0800

    asd

---
+++ js/other_login_register.js
@@ -0,0 +1,25 @@
+
+		$(document).ready(function() {
+			$("#login").click(function(){
+				$.XYTipsWindow({
+					___title:"登录",
+					___content:"iframe:../rights/login.html",
+					___width:"460",
+					___height:"150",
+					___showbg:true,
+					___drag:"___boxTitle"
+
+				});
+			});
+	
+			$("#reg").click(function(){
+				$.XYTipsWindow({
+					___title:"注册",
+					___content:"iframe:../rights/reg.html",
+					___width:"460",
+					___height:"450",
+					___showbg:true,
+					___drag:"___boxTitle"
+				});
+			});
+		});
